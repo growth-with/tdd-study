04e91dc6ea4ccd984ff2bf2a4ee5c725485e2c3b
Yunseo Hwang <yunseo.dev@gmail.com>
2022-03-06 21:53:46 +0900

    [문자열 계산기 4회차] 중위표기법 -> 후위표기법 변환 함수 추가

--- string-calculator/src/util.test.ts
+++ string-calculator/src/util.test.ts
@@ -1,4 +1,10 @@
-import { isOp, isNum, opFunc } from "./util";
+import {
+  isOp,
+  isNum,
+  opFunc,
+  convertInfixToPostfix,
+  opPriorityCompare,
+} from "./util";
 
 describe("isOp 함수 테스트", () => {
   it("+, -, *, /에 대해서 참을 반환해야 합니다.", () => {
@@ -40,10 +46,54 @@ describe("isNum 함수 테스트", () => {
 });
 
 describe("opFunc 함수 테스트", () => {
-    it("사칙연산이 작동해야 합니다.", () => {
-        expect(opFunc['+'](2,2)).toBe(4);
-        expect(opFunc['-'](2,2)).toBe(0);
-        expect(opFunc['/'](2,2)).toBe(1);
-        expect(opFunc['*'](2,2)).toBe(4);
+  it("사칙연산이 작동해야 합니다.", () => {
+    expect(opFunc["+"](2, 2)).toBe(4);
+    expect(opFunc["-"](2, 2)).toBe(0);
+    expect(opFunc["/"](2, 2)).toBe(1);
+    expect(opFunc["*"](2, 2)).toBe(4);
+  });
+});
+
+describe("opPriorityCompare 함수 테스트", () => {
+  it("연산자 우선순위가 뒤 인자가 더 높을 경우 -1을 반환해야 합니다.", () => {
+    expect(opPriorityCompare("+", "*")).toBe(-1);
+    expect(opPriorityCompare("-", "*")).toBe(-1);
+    expect(opPriorityCompare("+", "/")).toBe(-1);
+    expect(opPriorityCompare("-", "/")).toBe(-1);
+  });
+  it("연산자 우선순위가 동일할 경우 0을 반환해야 합니다.", () => {
+    expect(opPriorityCompare("+", "+")).toBe(0);
+    expect(opPriorityCompare("-", "-")).toBe(0);
+    expect(opPriorityCompare("*", "*")).toBe(0);
+    expect(opPriorityCompare("/", "/")).toBe(0);
+  });
+  it("연산자 우선순위가 앞 인자가 더 높을 경우 1을 반환해야 합니다.", () => {
+    expect(opPriorityCompare("*", "+")).toBe(1);
+    expect(opPriorityCompare("*", "-")).toBe(1);
+    expect(opPriorityCompare("/", "+")).toBe(1);
+    expect(opPriorityCompare("/", "-")).toBe(1);
+  });
+});
+
+describe("convertInfixToPostfix 함수 테스트", () => {
+  it("중위표기법 -> 후위표기법으로 변환해야 합니다.", () => {
+    const testCases = [
+      {
+        input: ["3", "+", "5"],
+        output: ["3", "5", "+"],
+      },
+      {
+        input: ["3", "+", "5", "*", "2", "/", "(", "7", "-", "2", ")"],
+        output: ["3", "5", "2", "*", "7", "2", "-", "/", "+"],
+      },
+      {
+        input: ["3", "*", "(", "5", "+", "2", ")", "-", "9"],
+        output: ["3", "5", "2", "+", "*", "9", "-"],
+      },
+    ];
+
+    testCases.forEach((testCase) => {
+      expect(convertInfixToPostfix(testCase.input)).toEqual(testCase.output);
     });
+  });
 });
--- string-calculator/src/util.ts
+++ string-calculator/src/util.ts
@@ -1,9 +1,9 @@
 export type OpType = "+" | "-" | "/" | "*";
 
 export const isOpType = (str: string): str is OpType => {
-  const opList = ['+', '-', '/', '*'];
+  const opList = ["+", "-", "/", "*"];
   return opList.includes(str);
-}
+};
 
 export const opFunc: Record<OpType, (a: number, b: number) => number> = {
   "+": (a: number, b: number): number => a + b,
@@ -20,4 +20,55 @@ export const isOp = (s: string): Boolean => {
 
 export const isNum = (s: string): Boolean => {
   return s !== " " && s !== "" && !isNaN(Number(s));
+};
+
+export const opPriorityCompare = (opA: string, opB: string): number => {
+  const highPriorityOp = ["*", "/"];
+
+  const priorityOfOpA = highPriorityOp.includes(opA) ? 2 : 1;
+  const priorityOfOpB = highPriorityOp.includes(opB) ? 2 : 1;
+
+  return priorityOfOpA - priorityOfOpB;
+};
+
+export const convertInfixToPostfix = (infix: string[]): string[] => {
+  const postfix: string[] = [];
+  const stack: string[] = [];
+
+  const moveStackTopItemToPostFix = () => {
+    const popItem = stack.pop();
+    if (popItem) {
+      postfix.push(popItem);
+    }
+  };
+
+  infix.forEach((v) => {
+    // 피연산자는 바로 push
+    if (isNum(v)) return postfix.push(v);
+
+    // 연산자 일 경우, 기존 stack에서 우선순위 같거나 높은 애들 push하고 자신을 stack에 추가
+    if (isOp(v)) {
+      for (let i = stack.length - 1; isOp(stack[i]); i -= 1) {
+        if (opPriorityCompare(stack[i], v) >= 0) {
+          moveStackTopItemToPostFix();
+        }
+      }
+      return stack.push(v);
+    }
+
+    if (v === "(") return stack.push(v);
+
+    // 닫는 괄호 일 경우 여는 괄호를 만날 때까지 push
+    if (v === ")") {
+      for (let i = stack.length - 1; stack[i] !== "(" && i >= 0; i -= 1) {
+        moveStackTopItemToPostFix();
+      }
+      return stack.pop();
+    }
+  });
+
+  // 남은 연산자들 push
+  stack.reverse().forEach((v) => postfix.push(v));
+
+  return postfix;
 };
